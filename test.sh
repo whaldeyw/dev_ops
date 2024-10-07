8759a3984bd1603f1a6d4d68a4848d1b6977964e
Oleg T <haldey86@mail.ru>
2024-10-07 04:55:45 +0300

    read version

--- test.sh
+++ test.sh
@@ -2,7 +2,17 @@
 
 list_files() {
     cd ./N
-    ls;
+    list=($(ls))
+    #echo ${list[*]}
+
+    
+    echo -n "Введите номер версии которую хотите удалить "
+    read version
+    name=${list[*]}
+    find  -name "*$version*" -delete;
+    echo "$version"
+  
+
 }
 
 list_files
